e0246a7015e10bbc9665ec0432b5990a41e7047e
devfelipelimabr <felipelima85@hotmail.com>
2023-11-03 11:07:35 -0300

    Created Home request API.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 import Header from "../../components/Header";
 
-function Home(){
-  return(
+// URL da API: movie/now_playing?api_key=f9fef8f91d4973d6573a77bfac45823f&language=pt-BR
+
+function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing",{
+        params: {
+          api_key: "f9fef8f91d4973d6573a77bfac45823f",
+          language: "pt-BR",
+          page: 1,
+        }
+      })     
+    }
+
+    loadFilmes();
+  }, []);
+
+  return (
     <>
-    <Header/>
+      <Header />
       <h1>BEM VINDO A HOME</h1>
     </>
-  )
+  );
 }
 
 export default Home;
