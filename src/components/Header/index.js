9940f636f7410fcfb6a9ccf1b1c0a606c92f6db2
devfelipelimabr <felipelima85@hotmail.com>
2023-11-04 14:24:01 -0300

    Changed header menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,17 +4,13 @@ function Header(){
         <>
         <header>
             <img className="logo" src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="logo"/>
+            <a href="/" className="site-name">FilmeZ</a>
         <ul>
                 <li>
                     <a href="/">
                         Home
                     </a>
-                </li>
-                <li>
-                    <a href="/filme/:id">
-                        Filmes
-                    </a>
-                </li>
+                </li>                
                 <li>
                     <a href="/favoritos">
                         Favoritos
