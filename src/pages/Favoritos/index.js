655d83bd85d68b7c52065a84128b990291a96b5b
devfelipelimabr <felipelima85@hotmail.com>
2023-11-05 13:39:09 -0300

    Created function on favorites page.

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
+
 import Header from "../../components/Header";
 
 function Favoritos(){
+const [filmes, setFilmes] = useState([]);
+
+useEffect(()=>{
+  const favoritos = localStorage.getItem("@filmez");
+  setFilmes(JSON.parse(favoritos) || [])
+},[])
+
   return(
     <>
     <Header/>
-    <div>
-      <h1>BEM VINDO A PAGINA DETALHES DOS FAVORITOS</h1>
+    <div className="container">
+      <ul className="lista-filmes">
+        {filmes.map((filme)=>{
+          return(            
+             <li key={filme.id}>
+              <div className="film">
+              <h2>{filme.title}</h2>             
+             <img className="film-poster" src={`https://image.tmdb.org/t/p/original${filme.poster_path}`} alt={filme.title}/>
+             <a className="large-btn" href={`/filme/${filme.id}`}>Acessar</a>
+             </div>
+             <span className="poster-imdb">imdb - {parseFloat(filme.vote_average).toFixed(1)} / 10</span>
+             </li>       
+          )
+        })}
+      </ul>
     </div>
     </>
   )
