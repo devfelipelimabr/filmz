e51d760449a3639b5df6ea225e02d7b7033ae1ca
devfelipelimabr <felipelima85@hotmail.com>
2023-11-11 14:24:58 -0300

    Created delete film button on favorite page.

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,38 +1,63 @@
-import { useEffect, useState } from "react";
-
+import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
-function Favoritos(){
-const [filmes, setFilmes] = useState([]);
+function Favoritos() {
+  const [filmes, setFilmes] = useState([]);
 
-useEffect(()=>{
-  const favoritos = localStorage.getItem("@filmez");
-  setFilmes(JSON.parse(favoritos) || [])
-},[])
+  useEffect(() => {
+    const favoritos = localStorage.getItem("@filmez");
+    setFilmes(JSON.parse(favoritos) || []);
+  }, []);
 
-  return(
+  const handleRemoverFilme = (id) => {
+    // Exibe uma janela de confirmação
+    const confirmacao = window.confirm("Tem certeza que deseja remover este filme?");
+    if (!confirmacao) {
+      return; // Se o usuário cancelar, não faz nada
+    }
+
+    // Filtra os filmes removendo o filme com o ID correspondente
+    const novosFilmes = filmes.filter((filme) => filme.id !== id);
+    setFilmes(novosFilmes);
+    // Atualiza o localStorage com os novos filmes
+    localStorage.setItem("@filmez", JSON.stringify(novosFilmes));
+  };
+
+  return (
     <>
-    <Header/>
-    <div className="container">
-      <ul className="lista-filmes">
-        {filmes.map((filme)=>{
-          return(            
-             <li key={filme.id}>
+      <Header />
+      <div className="container">
+        <ul className="lista-filmes">
+          {filmes.map((filme) => (
+            <li key={filme.id}>
               <div className="film">
-              <h2>{filme.title}</h2>             
-             <img className="film-poster" src={`https://image.tmdb.org/t/p/original${filme.poster_path}`} alt={filme.title}/>
-             <a className="large-btn" href={`/filme/${filme.id}`}>Acessar</a>
-             </div>
-             <span className="poster-imdb">imdb - {parseFloat(filme.vote_average).toFixed(1)} / 10</span>
-             </li>       
-          )
-        })}
-      </ul>
-    </div>
-    <Footer/>
+                <h2>{filme.title}</h2>
+                <img
+                  className="film-poster"
+                  src={`https://image.tmdb.org/t/p/original${filme.poster_path}`}
+                  alt={filme.title}
+                />
+                <a className="large-btn" href={`/filme/${filme.id}`}>
+                  Acessar
+                </a>
+                <button
+                  className="remove-btn"
+                  onClick={() => handleRemoverFilme(filme.id)}
+                >
+                  Remover
+                </button>
+              </div>
+              <span className="poster-imdb">
+                imdb - {parseFloat(filme.vote_average).toFixed(1)} / 10
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default Favoritos;
